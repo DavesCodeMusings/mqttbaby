539160e7769a22ef4972fb367a496c84a32caea8
Dave <61114342+DavesCodeMusings@users.noreply.github.com>
2021-08-01 16:35:21 -0500

    Added license information

--- mqttbaby.ino
+++ mqttbaby.ino
@@ -1,5 +1,31 @@
 /* mqttbaby 
- * For ESP8266, NodeMCU, D1, and clones. Publish data to MQTT topics.
+ * For ESP8266, NodeMCU, D1, and clones. Publish sensor data to MQTT topics.
+ * https://github.com/DavesCodeMusings/mqttbaby
+ * 
+ * Copyright (c) 2021, David Horton
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
  */
 #include <FS.h>
 #include <ESP8266WiFi.h>
@@ -390,7 +416,7 @@ void setup() {
   digitalWrite(LED_BUILTIN, LOW);
   Serial.begin(SERIAL_BPS);
   Serial.println();
-  Serial.println("MQTT Baby");
+  Serial.println("MQTT Baby (c)2021 David Horton");
   Serial.println("https://github.com/DavesCodeMusings/mqttbaby");
   Serial.println();
   
